7fcea58ba02df4228e511b0f40ee6efa87a69115
AgnesHager <nen018@uib.no>
2026-03-05 15:09:23 +0100

    Create BW_correlation.R

---
+++ BW_correlation.R
@@ -0,0 +1,4 @@
+# Checking to see if gene X is associated with Delta BW (so body weight gain)
+# Using the DEGs as genes I correlate to
+# Using Spearman correlation, to account for non-normality and potential outliers
+
